9e99b0305bb5751d96f04705afdfd7413a16276c
gaowei1012 <mingwei2222@126.com>
2020-08-31 13:57:04 +0800

    集成antd

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
+import { Button } from 'antd'
 import './index.less'
 
 const About = () => {
     return (
         <div className='aboutConatiner'>
             about
+            <Button type='ghost'>按钮</Button>
         </div>
     )
 }
