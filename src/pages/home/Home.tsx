ab793775a226585e65792f8dd27194a10704b224
gaowei1012 <mingwei2222@126.com>
2020-08-31 17:13:29 +0800

    hooks使用

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,31 @@
 import * as React from 'react'
+import { useState, useCallback, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Button, message } from 'antd'
 import './index.less'
 
+interface Pepole {
+    name: String,
+    age: Number,
+    desc?: String
+}
+
 const Home = (): React.ReactElement => {
+
+    const [count, setCount] = useState<Number>(0)
+    const [arr, setArr] = useState<[]>([])
+    const [add, setAdd] = useState<Number>(0)
+
+    const [user, setUser] =useState<Pepole>({name: 'age', age: 12, desc: 'test'})
+
+    useEffect(() => {
+        // TODO
+    }, [])
+
     return (
         <div className='homeContainer'>
-            home
+            <span>{count}</span>
+            <Button type='ghost'>增加</Button>
         </div>
     )
 }
